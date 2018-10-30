672600e54a58a09d5f02c7b685ac6c5b5cf93778
Mystic <mysteriousGE@gmail.com>
2018-10-30 21:59:20 +0800

    [FAIL] Is 3 is power of 2?

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -8,3 +8,8 @@ class test_Calculator(unittest.TestCase):
         calculator = Calculator.Calculator()
         actual = calculator.is_power_of_two(2)
         self.assertTrue(actual)
+
+    def test_is_power_Of_two__Input_3_Return_False(self):
+        calculator = Calculator.Calculator()
+        actual = calculator.is_power_of_two(3)
+        self.assertFalse(actual)
