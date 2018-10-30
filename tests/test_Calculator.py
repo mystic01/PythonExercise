c85ede2277d723265657e5aee982b6ac194289f5
Mystic <mysteriousGE@gmail.com>
2018-10-30 22:10:51 +0800

    [REFACTOR] Refactoring unit test.

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -4,12 +4,11 @@ from PythonExercise import Calculator
 
 class test_Calculator(unittest.TestCase):
 
-    def test_is_power_Of_two__Input_2_Return_True(self):
-        calculator = Calculator.Calculator()
-        actual = calculator.is_power_of_two(2)
-        self.assertTrue(actual)
+    def setUp(self):
+        self.calculator = Calculator.Calculator()
 
-    def test_is_power_Of_two__Input_3_Return_False(self):
-        calculator = Calculator.Calculator()
-        actual = calculator.is_power_of_two(3)
-        self.assertFalse(actual)
+    def test_is_power_of_two__Input_2_Return_True(self):
+        self.assertTrue(self.calculator.is_power_of_two(2))
+
+    def test_is_power_of_two__Input_3_Return_False(self):
+        self.assertFalse(self.calculator.is_power_of_two(3))
