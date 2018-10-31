407e55d80f28f0a5747e315a67e45af1fb2470b5
Mystic <mysteriousGE@gmail.com>
2018-10-31 21:58:48 +0800

    [PASS] error format case.

--- PythonExercise/Parser.py
+++ PythonExercise/Parser.py
@@ -5,7 +5,7 @@ class Parser:
     def Parse(self, product_code):
         is_match = bool(re.match(r"[\w|\d]+-\d+\(\d+.\d+\)$", product_code))
         if not is_match:
-            return
+            return None
         rgx = re.compile(r"[\w|\d]+")
         result = rgx.findall(product_code)
         return result[0], result[1], result[2], str(int(result[3])/10)
--- tests/test_Parser.py
+++ tests/test_Parser.py
@@ -4,7 +4,7 @@ from PythonExercise import Parser
 
 class test_Parser(unittest.TestCase):
 
-    def test_ParseVersionString(self):
+    def test_ParseVersionString_normal_case(self):
         parser = Parser.Parser()
         model_name, date_code, major_version, minor_version = parser.Parse(
             "PLC320-1810310001(1.05)")
@@ -12,3 +12,8 @@ class test_Parser(unittest.TestCase):
         self.assertEqual("1810310001", date_code)
         self.assertEqual("1", major_version)
         self.assertEqual("0.5", minor_version)
+
+    def test_ParseVersionString_error_case_return_none(self):
+        parser = Parser.Parser()
+        result = parser.Parse("PLC320-1810310001(1.05))")
+        self.assertIsNone(result)
