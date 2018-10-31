d59e7fe4928495b4cf15362dea10a813c792dc4a
Mystic <mysteriousGE@gmail.com>
2018-10-31 21:51:18 +0800

    [PASS] pass.

---
+++ PythonExercise/Parser.py
@@ -0,0 +1,11 @@
+import re
+
+
+class Parser:
+    def Parse(self, product_code):
+        is_match = bool(re.match(r"[\w|\d]+-\d+\(\d+.\d+\)$", product_code))
+        if not is_match:
+            return
+        rgx = re.compile(r"[\w|\d]+")
+        result = rgx.findall(product_code)
+        return result[0], result[1], result[2], str(int(result[3])/10)
