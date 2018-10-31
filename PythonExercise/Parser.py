942cbde9087d7d4fa03f9ddf699f5351acbdb1a4
Mystic <mysteriousGE@gmail.com>
2018-10-31 22:53:55 +0800

    [REFACTORING]

--- PythonExercise/Parser.py
+++ PythonExercise/Parser.py
@@ -1,6 +1,14 @@
 import re
 
 
+class ProductInfo:
+    def __init__(self, model_name, date_code, major_version, minor_version):
+        self.model_name = model_name
+        self.date_code = date_code
+        self.major_version = major_version
+        self.minor_version = minor_version
+
+
 class Parser:
     def Parse(self, product_code):
         is_match = bool(re.match(r"[\w|\d]+-\d+\(\d+.\d+\)$", product_code))
@@ -8,4 +16,5 @@ class Parser:
             return None
         rgx = re.compile(r"[\w|\d]+")
         result = rgx.findall(product_code)
-        return result[0], result[1], result[2], str(int(result[3])/10)
+        minor_verison = str(int(result[3])/10)
+        return ProductInfo(result[0], result[1], result[2], minor_verison)
--- tests/test_Parser.py
+++ tests/test_Parser.py
@@ -8,13 +8,16 @@ class test_Parser(unittest.TestCase):
         self.parser = Parser.Parser()
 
     def test_ParseVersionString_normal_case(self):
-        model_name, date_code, major_version, minor_version = self.parser.Parse(
-            "PLC320-1810310001(1.05)")
-        self.assertEqual("PLC320", model_name)
-        self.assertEqual("1810310001", date_code)
-        self.assertEqual("1", major_version)
-        self.assertEqual("0.5", minor_version)
+        product_info = self.parser.Parse("PLC320-1810310001(1.05)")
+        self.assertEqual("PLC320", product_info.model_name)
+        self.assertEqual("1810310001", product_info.date_code)
+        self.assertEqual("1", product_info.major_version)
+        self.assertEqual("0.5", product_info.minor_version)
 
     def test_ParseVersionString_error_case_return_none(self):
-        result = self.parser.Parse("PLC320-1810310001(1.05))")
-        self.assertIsNone(result)
+        product_info = self.parser.Parse("PLC320-1810310001(1.05))")
+        self.assertIsNone(product_info)
+
+    def test_ParseVersionString_error_case2_return_none(self):
+        product_info = self.parser.Parse("PLC320--1810310001(1.05)")
+        self.assertIsNone(product_info)
